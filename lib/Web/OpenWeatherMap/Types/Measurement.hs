d81031d9389d1fd3c7450650f9f5d56d678b60b8
Jappie Klooster <jappieklooster@hotmail.com>
2020-04-17 14:02:22 +0200

    Parse the date coming from the weather data api immediatly

--- lib/Web/OpenWeatherMap/Types/Measurement.hs
+++ lib/Web/OpenWeatherMap/Types/Measurement.hs
@@ -1,18 +1,29 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Web.OpenWeatherMap.Types.Measurement (
   Measurement (..)
 ) where
 
 import GHC.Generics (Generic)
-import Data.Aeson (FromJSON)
+import Data.Aeson 
 import Web.OpenWeatherMap.Types.Main
-import Web.OpenWeatherMap.Types.Weather 
+import Web.OpenWeatherMap.Types.Weather
+import Data.Time
+import Data.Text
 
 data Measurement = Measurement {
   main :: Main,
   weather :: [Weather],
-  dt_txt :: String
-  } deriving (Show, Generic, FromJSON)
+  dt_txt :: UTCTime
+} deriving (Show, Generic)
 
+instance FromJSON Measurement where
+  parseJSON = 
+    withObject "Measurement" $ \v -> do
+      result <- v .: "dt_txt"
+      Measurement 
+          <$> v .: "main"
+          <*> v .: "weather"
+          <*> (parseTimeM True defaultTimeLocale "%Y-%-m-%-d %H:%M:%S" result)
